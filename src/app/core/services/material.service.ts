e1309eb84ed3cb3133ae3b8b3b4aea8782294d9c
German Fica <germafica@gmail.com>
2022-05-11 20:17:40 -0300

    Update material service

--- src/app/core/services/material.service.ts
+++ src/app/core/services/material.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MaterialService {
+  // Observable string sources
+  private materialSource = new Subject<string>();
+
+  // Observable string streams
+  material$ = this.materialSource.asObservable();
 
   constructor() { }
+
+  // Service message commands
+  sendMaterial(mat: string) {
+    this.materialSource.next(mat); // send material
+  }
 }
