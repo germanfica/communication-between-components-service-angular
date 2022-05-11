8cd14efb2daad4e9aba1f819bcc6f7259ec25916
German Fica <germafica@gmail.com>
2022-05-11 20:22:05 -0300

    Add a, b, c components

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MaterialsRoutingModule } from './materials-routing.module';
 import { MaterialsComponent } from './components/materials/materials.component';
+import { AComponent } from './components/a/a.component';
+import { BComponent } from './components/b/b.component';
+import { CComponent } from './components/c/c.component';
 
 
 @NgModule({
   declarations: [
-    MaterialsComponent
+    MaterialsComponent,
+    AComponent,
+    BComponent,
+    CComponent
   ],
   imports: [
     CommonModule,
