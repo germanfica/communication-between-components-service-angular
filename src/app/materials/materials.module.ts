327c5ce0f9fb95f7a08402e21ae45a85acd1f1de
German Fica <germafica@gmail.com>
2022-05-11 19:58:15 -0300

    Add materials component

---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialsRoutingModule } from './materials-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialsRoutingModule
+  ]
+})
+export class MaterialsModule { }
